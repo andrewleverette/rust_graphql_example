617a2dceef3182252481acf8218693f510a94aa8
Andrew Leverette <andrewleverette@gmail.com>
2020-09-23 15:56:39 -0500

    Add comments

--- src/schema.rs
+++ src/schema.rs
@@ -3,7 +3,9 @@ use juniper::{self, EmptyMutation, RootNode};
 pub struct Query;
 
 #[juniper::object]
+/// An example GraphQL Schema built with Rust
 impl Query {
+    /// Simple "Hello, world!" query
     fn hello_world() -> &str {
         "Hello, world!"
     }
